0c46b103c09c45d76560a66d369736850288b3fe
suraj.kumar <surajumang08@gmail.com>
2020-03-25 14:10:04 +0530

    remove copyright info

--- src/main/java/com/care/impl/transformer/JsonStringTransformer.java
+++ src/main/java/com/care/impl/transformer/JsonStringTransformer.java
@@ -1,13 +1,3 @@
-/*
- * Copyright 2006-2020 (c) Care.com, Inc.
- * 1400 Main Street, Waltham, MA, 02451, U.S.A.
- * All rights reserved.
- *
- * This software is the confidential and proprietary information of
- * Care.com, Inc. ("Confidential Information").  You shall not disclose
- * such Confidential Information and shall use it only in accordance with
- * the terms of an agreement between you and CZen.
- */
 package com.care.impl.transformer;
 
 import com.care.jsontemplate.ExpressionEvaluator;
